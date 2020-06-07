0643634793f517f869ef7a091cace74b5804dc77
Patryk <patrickwww94@gmail.com>
2020-06-07 13:39:17 +0200

    Initial commit

---
+++ backend/src/main/java/com/example/reactiveshop/ReactiveShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactiveshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveShopApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/reactiveshop/ReactiveShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
